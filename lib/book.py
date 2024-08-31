6b503e04e60f14ae314afb66f85273db899396d3
Kalvine <kkalvine4@gmail.com>
2024-08-31 11:30:37 +0300

    Puts everything together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="And Then There Were None", page_count=272):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        '''The title property'''
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        print(f"Setting title to {title}")
+        self._title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+
+        if isinstance(page_count, int):
+            print(f"Setting page_count to {page_count}")
+            self._page_count = page_count 
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        '''Outputs a message indicating that the page has been turned.'''
+        print("Flipping the page...wow, you read fast!")
+        
+               
+
+book = Book("And Then There Were None", 272)
+book.title = "The good place"
+book.page_count = 1322
+book.turn_page()
+print(book.title, book.page_count)
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "Adidas", size = 9):
+        self.brand = brand
+        self.size = size
+        
+
+    @property
+    def brand(self):
+        '''The brand property'''
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        print(f"Setting brand to {brand}")
+        self._brand = brand
+
+    @property
+    def size(self):
+        '''The brand property'''
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+
+        if isinstance(size, int):
+            print(f"Setting size to {size}")
+            self._size = size
+
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        '''Sets the condition after repair.'''
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+        
+
+
+shoe = Shoe("Nike", 11)
+shoe.brand = "Under Armour"
+shoe.size = 8
+shoe.cobble()
+print(shoe.brand, shoe.size)
